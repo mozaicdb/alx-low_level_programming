6f67f6739384f44470a01d0d05f0ab6d577e5ca5
mozaicdb <mozaic138@gmail.com>
2023-07-14 20:50:39 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,8 +11,12 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes here */
+/* your code goes there */
 if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+if (n < 0)
 {
 printf("%d is negative\n", n);
 }
