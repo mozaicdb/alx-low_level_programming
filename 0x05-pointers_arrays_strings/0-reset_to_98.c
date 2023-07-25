9eae1ecce153c783c11f96c2395ae65d13cecf76
mozaicdb <mozaic138@gmail.com>
2023-07-24 18:54:43 -0700

    added correction

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * rest_to_98 - takes the pointer and updates the values
- * @n: int to check
- * owned By mozaicteck
- * Return: 0 is success
+ * reset_to_98 - update address function
+ *@n: intege
+ *
  */
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
