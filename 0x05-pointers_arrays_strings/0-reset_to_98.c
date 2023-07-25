1f2d57331ceaabf92f8f6b648293e33e6b484798
mozaicdb <mozaic138@gmail.com>
2023-07-24 18:44:48 -0700

    0 98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * rest_to_98 - takes the pointer and updates the values
+ * @n: int to check
+ * owned By mozaicteck
+ * Return: 0 is success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
