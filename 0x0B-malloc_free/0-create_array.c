354f259f7319491057e1290a015c1f47d99d81eb
mozaicdb <mozaic138@gmail.com>
2023-08-07 13:37:57 -0700

    0-create

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,26 +1,28 @@
-[200~#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- *  * create_array - Creates an array of size `size` and assigns char `c`.
- *   * @size: Size of the array.
- *    * @c: Char to assign.
- *     *
- *      * Description: Creates an array of size `size` and assigns char `c`.
- *       * Return: Pointer to the array, NULL if fail.
- *        */
+ * create_array - function that creates an array of chars, and initializes it
+ *                with a specific char.
+ *
+ * @size: This is the length of the array
+ * @c: This is the input character
+ *
+ * Return: An Array Initialized with the specific char
+ */
+
 char *create_array(unsigned int size, char c)
 {
-		char *str;
-			unsigned int i;
+	unsigned int a;
+	char *s;
 
-				str = malloc(sizeof(char) * size);
-
-					if (size == 0 || str == NULL)
-								return (NULL);
-
-						for (i = 0; i < size; i++)
-									str[i] = c;
-
-							return (str);
+	s = (char *) malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (a = 0; a < size; a++)
+	{
+		s[a] = c;
+	}
+	return (s);
 }
